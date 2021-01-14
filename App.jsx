3620ca8dc39fac4d5c38054bd7e3b1ba0525a10e
flis_ann <ann.flismark@gmail.com>
2021-01-14 19:47:25 +0100

    Adding @react-navigation/native, adding articles to initialState

--- App.jsx
+++ App.jsx
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { useSelector } from 'react-redux'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const App = () => {
   const { appTitle } = useSelector(state => state)
